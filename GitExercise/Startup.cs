c868773338cf7b1888468dc833d0a75e46a2b725
PetarDanailov <danailovvpetar@gmail.com>
2024-10-22 17:52:28 +0300

    Division,abs substraction add

--- GitExercise/Startup.cs
+++ GitExercise/Startup.cs
@@ -6,6 +6,13 @@ namespace GitExercise
     {
         public static void Main()
         {
+            bool isAuthorized = CheckCredentials();
+            if (!isAuthorized)
+            {
+                Console.WriteLine("Access denied.");
+                Console.ReadKey(true);
+                return;
+            }
             Console.WriteLine("Console Calculator App");
             Console.WriteLine(new string('-', 15));
 
@@ -39,6 +46,15 @@ namespace GitExercise
 
             Console.WriteLine("Pres any key to close the app...");
             Console.ReadKey(true);
+           
+        }
+        private const string Password = "abcd1234";
+        private static bool CheckCredentials()
+        {
+            Console.WriteLine("Enter password to gain access:");
+            string password = Console.ReadLine();
+            Console.Clear();
+            return password == Password;
         }
     }
 }
